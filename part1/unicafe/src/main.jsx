5bb817940c3363193c09d82801e8f88aa1f52cae
daniekCodes <daniekschuiling@gmail.com>
2026-04-17 13:52:42 +0200

    Scratch exercise Part 1

--- part1/unicafe/src/main.jsx
+++ part1/unicafe/src/main.jsx
@@ -1,4 +1,4 @@
 import ReactDOM from "react-dom/client"
-import App from './App'
-
-ReactDOM.createRoot(document.getElementById("root")).render(<App />)
+// import App from './App'
+import Scratch from './Scratch'
+ReactDOM.createRoot(document.getElementById("root")).render(<Scratch />)
